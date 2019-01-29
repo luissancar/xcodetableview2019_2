7f6f3efaa2bc915245378df746e52da5c6ae1a04
luis jose sanchez carrasco <luissancar@yahoo.es>
2019-01-29 12:30:49 +0100

    Add files via upload

---
+++ tableview2019_2/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  tableview2019_2
+//
+//  Created by Nosferatu on 29/01/2019.
+//  Copyright © 2019 luissancar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+    let data:[String]=["fila 1","fila 2","fila 3"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell=UITableViewCell()
+       
+        cell.textLabel?.text=data[indexPath.row]
+        return cell
+    }
+
+}
+
